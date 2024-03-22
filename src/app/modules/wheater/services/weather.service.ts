7598d8e07b63f305d31ac2ec1146bd95f86a7a38
isaacam517 <isaacam517@gmail.com>
2024-03-22 11:39:07 -0300

    first commit

---
+++ src/app/modules/wheater/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = '5b194d9078810a32f7b689e607adf6c6';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+     {})
+  }
+}
